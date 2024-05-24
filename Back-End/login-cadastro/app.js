a2293007500b0ec8e831f92b06c43ad965ac6c93
IsadoraOliveira24 <isadorabz240706@gmail.com>
2024-05-24 14:14:33 -0300

    AAAAAA

--- Back-End/login-cadastro/app.js
+++ Back-End/login-cadastro/app.js
@@ -1,63 +1,81 @@
 const mysql = require('mysql2');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const db = mysql.createConnection({
     host: 'localhost',
-    user:'isadora',
-    password:'senai@2024',
-    database:'LoginMayara'
+    user: 'Isadora',
+    password: 'Senai@2024',
+    database: 'login'
 });
 
-db.connect((error)=>{
-    if(error){
-        console.log("Erro ao conectar com banco de dados")
+db.connect((error) => {
+    if (error) {
+        console.error("Erro ao conectar com banco de dados:", error);
     } else {
         console.log("Conectado ao MySQL");
     }
 });
 
-const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + '/login.html')
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
 
-app.post("/login", (req, res)=>{
-    console.log(req.body)
-    const username = req.body.usuario
-    const password =req.body.senha
+app.get("/cadastro", (req, res) => {
+    res.sendFile(__dirname + '/cadastro.html');
+});
 
-    
+app.post("/login", (req, res) => {
+    console.log(req.body);
+    const username = req.body.username;
+    const password = req.body.password;
 
     db.query('SELECT password FROM user WHERE username = ?', [username], (error, results) => {
-       
-       if (results.length>0){
-        const passwordDB = results[0].password;
-        const userDB = results[0].username;
-        if(password === passwordDB){
-            console.log("Bem vinda Isadora")
-            res.redirect("/welcome.html")
-         } else {
-            console.log("Senha não reconhecida")
-            res.redirect("/Senha_Errada.html")
-         }
-       } else {
-        console.log("Usuario não cadastrado")
-        res.redirect("/Cadastrar.html")
-       }
-     
-     
-     })
-})
+        if (error) {
+            console.error("Erro ao executar query:", error);
+            res.redirect("/ErroQuery.html");
+        } else if (results.length > 0) {
+            const passwordDB = results[0].password;
+            if (password === passwordDB) {
+                console.log("Bem vinda Isadora");
+                res.redirect("/welcome.html");
+            } else {
+                console.log("Senha incorreta");
+                res.redirect("/senhaincorreta");
+            }
+        } else {
+            console.log("Usuario não cadastrado");
+            res.redirect("/Tente_Novamente.html");
+        }
+    });
+});
 
-db.query('insert into user', [username, email, senha], (error, results)=>
-    
-)
+app.post("/cadastro", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const confirm = req.body.passwordConfirm;
 
-app.listen(port, ( )=>{
-    console.log (`Servidor rodando no enedreço: http://localhost:${port}`)
-})
+    if (password === confirm) {
+        db.query('INSERT INTO user (username, password) VALUES (?, ?)', [username, password], (error, results) => {
+            if (error) {
+                console.error("Erro ao cadastrar usuário:", error);
+                res.redirect("/Nao_Deu_Certo.html");
+            } else {
+                console.log("Usuário cadastrado com sucesso", results);
+                res.redirect('/');
+            }
+        });
+    } else {
+        console.log('Senhas não coincidem');
+        res.redirect("/SenhasNaoCoincidem.html");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando no endereço: http://localhost:${port}`);
+});
